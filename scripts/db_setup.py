54b26df9e4bb60ba142da77d19a4bf4f0461b349
Marcus1911 <sandri.sdn@gmail.com>
2024-03-31 16:14:57 -0300

    FIX: pipeline issue

--- scripts/db_setup.py
+++ scripts/db_setup.py
@@ -11,7 +11,7 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 #         f"host={os.getenv('POSTGRES_HOST')}"
 #     )
 
-connection_details = 'dbname=directus user=calcom password=123456 host=localhost'
+connection_details = 'dbname=directus user=calcom password=123456 host=postgres'
 conn = psycopg2.connect(connection_details)
 conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 
